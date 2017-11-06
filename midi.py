104aaccd3df80ce2eee14e0bea76dd30f06579c4
Daniel Gullberg <daniel_gullberg@hotmail.com>
2017-11-06 23:15:06 +0100

    Using pygame to play sounds

--- midi.py
+++ midi.py
@@ -1,7 +1,10 @@
+#! coding: UTF-8
 import pygame.midi
 import time
 
 PROGRAM_CHANGE = 0xC0
+
+
 def handle_events(events):
     """Handle MIDI events"""
     for event in events:
@@ -11,16 +14,29 @@ def handle_events(events):
         if cmd == PROGRAM_CHANGE:
             progNum = data[1]
             print "Program number {}".format(progNum)
+            if progNum == 0:
+                print "Regn..."
+                pygame.mixer.Sound.play(soundRain)
+            if progNum == 1:
+                print "Åska!!!"
+                pygame.mixer.Sound.play(soundThunder)
             
     
 if __name__ == "__main__":
     pygame.midi.init()
+    pygame.mixer.init()
+    soundRain = pygame.mixer.Sound(r"/home/daniel/Music/Treo/Regn.wav")
+    soundThunder = pygame.mixer.Sound(r"/home/daniel/Music/Treo/Åska.wav")
+
     print pygame.midi.get_default_output_id()
     for i in range(pygame.midi.get_count()):
         info = pygame.midi.get_device_info(i)
         if info[2]:
             print "{}: Input: {}".format(i, info)
     midiIn = pygame.midi.Input(5)
+  #  pygame.mixer.music.load(r"/home/daniel/Music/Treo/Åska.wav")
+  #  pygame.mixer.music.load(r"/home/daniel/Music/Treo/Treo-Falling_en_inspelning.mp3")
+    
     while True:
         events = midiIn.read(10)
         handle_events(events)
